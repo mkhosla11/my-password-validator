a85993b16190d6ef8bbad2d3fcb8b21b349c0c12
Mo <monakhosla@lev-14236.apn.wlan.private.upenn.edu>
2025-03-25 14:39:54 -0400

    Mona

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import flask
 
 
 # TODO: change this to your academic email
-AUTHOR = "lumbroso@seas.upenn.edu"
+AUTHOR = "mkhosla@seas.upenn.edu"
 
 
 app = flask.Flask(__name__)
@@ -26,4 +26,17 @@ def check_password():
     pw = data.get("password", "")
 
     # FIXME: to be implemented
-    return flask.jsonify({"valid": False, "reason": "Not implemented"}), 501
+    uppercase = sum(1 for c in pw if c.isupper())
+    digit = sum(1 for c in pw if c.isdigit())
+    special = sum(1 for c in pw if c in '!@#$%^&*')
+
+    if len(pw) < 8:
+        return flask.jsonify({"valid": False, "reason": "Too short"}), 501
+    if uppercase < 2:
+        return flask.jsonify({"valid": False, "reason": "Not enough Upper Case Letters"}), 501
+    if digit < 2:
+        return flask.jsonify({"valid": False, "reason": "Not enough Digits"}), 501
+    if special < 1:
+        return flask.jsonify({"valid": False, "reason": "No special character"}), 501
+    return flask.jsonify({"valid": True, "reason": "Password is Good"}), 200
+
